f0f507feba7f32e60e4f7e69d412f050f8e22ca0
agung anggara <100152186+agunganggara22@users.noreply.github.com>
2025-01-03 10:51:13 +0700

    Add files via upload

---
+++ data/Lahan_sawit_FeatureToPoint_24.js
@@ -0,0 +1,1 @@
+var json_Lahan_sawit_FeatureToPoint_24 = {"type":"FeatureCollection","name":"Lahan_sawit_FeatureToPoint_24","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
